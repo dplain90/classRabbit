7f39c66ee2630cda60f6843c438d0f01086e92d0
Danny <dplain@gmail.com>
2017-04-19 13:35:20 -0400

    Complete login form refactor

--- frontend/components/sessions/session_form.jsx
+++ frontend/components/sessions/session_form.jsx
@@ -14,8 +14,9 @@ class sessionForm extends React.Component {
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.update = this.update.bind(this);
-    this.populateForm = this.populateForm.bind(this);
     this.renderErrors = this.renderErrors.bind(this);
+    this.zipCodeInput = this.zipCodeInput.bind(this);
+    this.nameInputs = this.nameInputs.bind(this);
   }
 
   componentDidMount(){
@@ -35,39 +36,6 @@ class sessionForm extends React.Component {
         }
       </ul>
     );
-  }
-
-  signUpForm(){
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input type="text" value={this.state.fname} onChange={this.update("fname")} />
-        <input type="text" value={this.state.lname} onChange={this.update("lname")} />
-        <input type="text" value={this.state.email} onChange={this.update("email")} />
-        <input type="password" value={this.state.password} onChange={this.update("password")} />
-        <input type="text" value={this.state.zip_code} onChange={this.update("zip_code")} />
-        <button type="submit"> Submit </button>
-      </form>
-    );
-  }
-
-  logInForm(){
-    console.log(this.state);
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input type="text" value={this.state.email} onChange={this.update("email")} />
-        <input type="password" value={this.state.password} onChange={this.update("password")} />
-        <button type="submit"> Submit </button>
-      </form>
-    );
-  }
-
-  populateForm() {
-    if(this.props.formType === 'signup') {
-      return this.signUpForm();
-    }
-    else {
-      return this.logInForm();
-    }
   }
 
   componentDidUpdate() {
@@ -95,34 +63,33 @@ class sessionForm extends React.Component {
     };
   }
 
+  nameInputs() {
+    if(this.props.isSignUp){
+      return (
+        <label className="name-fields">
+          <input type="text" value={this.state.fname} onChange={this.update("fname")} />
+          <input type="text" value={this.state.lname} onChange={this.update("lname")} />
+        </label>
+      );
+    }
+  }
 
+  zipCodeInput() {
+    if(this.props.isSignUp){
+      return (
+          <input type="text" value={this.state.zip_code} onChange={this.update("zip_code")} />
+      );
+    }
+  }
 
   render(){
-    const nameInputs = () => {
-
-      if(this.props.isSignUp){
-        return (
-          <label className="name-fields">
-            <input type="text" value={this.state.fname} onChange={this.update("fname")} />
-            <input type="text" value={this.state.lname} onChange={this.update("lname")} />
-          </label>
-        );
-      }};
-
-    const zipCodeInput = () => {
-      if(this.props.isSignUp){
-        return (
-            <input type="text" value={this.state.zip_code} onChange={this.update("zip_code")} />
-        );
-      }
-    };
     return (
       <div className="loginForm">
         <form onSubmit={this.handleSubmit}>
-          { nameInputs() }
+          { this.nameInputs() }
           <input type="text" value={this.state.email} onChange={this.update("email")} />
           <input type="password" value={this.state.password} onChange={this.update("password")} />
-          { zipCodeInput() }
+          { this.zipCodeInput() }
           <button type="submit"> {this.props.buttonText} </button>
         </form>
           { this.renderErrors() }
