5bb26ab38c3658dc570554402eda77df1efadf70
Danny <dplain@gmail.com>
2017-04-20 17:14:16 -0400

    Style dashboard easilyBook

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -36,48 +36,51 @@ class Dashboard extends React.Component {
 
     return (
       <div className="main">
-        <section className="search">
+        <div className="main-top">
+          <section className="search">
 
-          <div className="welcome">
-            Welcome, {this.props.user.fname}!
-              <img src={this.props.user.img_url} className="prof-pic" />
-          </div>
+            <div className="welcome">
+              Welcome, {this.props.user.fname}!
+                <img src={this.props.user.img_url} className="prof-pic" />
+            </div>
 
-          <div className="search-bar">
-            <i className="mag-glass"></i>
-            <input type="text" className="search-bar input" />
-            <i className="cancel"></i>
-          </div>
-        </section>
-        <section className="get-started">
+            <div className="search-bar">
+              <i className="mag-glass"></i>
+              <input type="text" className="search-bar input" />
+              <i className="cancel"></i>
+            </div>
+          </section>
+          <section className="get-started">
 
-        </section>
+          </section>
 
-        <section className="manage-tasks">
-          <div className="manage-task background outer-container">
-            <div className="manage-task background inner-container">
-              <h3>Easily book n manage tasks n our app</h3>
-              <p>Please enter your mobile number to receive a download link.</p>
-              <div className="manage-tasks send-text-form">
-                <input type="text" name="" value="" className="send-text-form" />
-                <button type="submit" name="button" className="send-text-form">Send Text</button>
+          <section id="manage-tasks">
+            <div className="manage-task-container">
+              <div className="manage-task-background">
+                <h3>Easily book n manage tasks n our app</h3>
+                <p>Please enter your mobile number to receive a download link.</p>
+                <div className="manage-tasks send-text-form">
+                  <input type="text" name="" value="" className="send-text-form" />
+                  <button type="submit" name="button" className="send-text-form">Send Text</button>
+                </div>
               </div>
             </div>
-          </div>
-        </section>
-
-        <section className="can-we-help">
-          <div className="can-we-help heading">
-            How We Can Help:
-          </div>
-          <div className="can-we-help">
-            <ul className="can-we-help list">
-              { this.favoriteCategories() }
-            </ul>
-          </div>
-        </section>
-        <section className="support-center">
-        </section>
+          </section>
+        </div>
+        <div className="main-bottom">
+          <section className="can-we-help">
+            <div className="can-we-help heading">
+              How We Can Help:
+            </div>
+            <div className="can-we-help">
+              <ul className="can-we-help list">
+                { this.favoriteCategories() }
+              </ul>
+            </div>
+          </section>
+          <section className="support-center">
+          </section>
+        </div>
       </div>
     );
   }
