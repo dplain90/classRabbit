90961599c9e3d084a94ddbd10675cb9a10588b43
Danny <dplain@gmail.com>
2017-04-21 12:15:22 -0400

    Add search styling

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -20,10 +20,10 @@ class Search extends React.Component {
     }
   }
 
-
   componentDidMount(){
   }
   // eventually build with a callback...
+  // need to fix to iterate through full word 
   filterResults(val){
     let filteredResults = [];
     const keys = Object.keys(this.state.results);
