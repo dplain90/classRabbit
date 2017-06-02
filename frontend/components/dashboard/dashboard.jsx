6d704fc671a7e6cb757d1b0f02515c882a2b939d
dplain90 <dplain@gmail.com>
2017-06-02 14:19:03 -0400

    Respace favorite categories

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -61,18 +61,7 @@ class Dashboard extends React.Component {
           </section>
           <RequestedTasksContainer />
 
-          <section id="manage-tasks">
-            <div className="manage-task-container">
-              <div className="manage-task-background">
-                <h3>Easily book n manage tasks n our app</h3>
-                <p>Please enter your mobile number to receive a download link.</p>
-                <div className="manage-tasks send-text-form">
-                  <input type="text" name="" value="" className="send-text-form" />
-                  <button type="submit" name="button" className="send-text-form">Send Text</button>
-                </div>
-              </div>
-            </div>
-          </section>
+      
         </div>
         <div className="main-bottom">
           <section className="can-we-help">
---
+++ frontend/components/dashboard/text.jsx
@@ -0,0 +1,12 @@
+<section id="manage-tasks">
+  <div className="manage-task-container">
+    <div className="manage-task-background">
+      <h3>Easily book n manage tasks n our app</h3>
+      <p>Please enter your mobile number to receive a download link.</p>
+      <div className="manage-tasks send-text-form">
+        <input type="text" name="" value="" className="send-text-form" />
+        <button type="submit" name="button" className="send-text-form">Send Text</button>
+      </div>
+    </div>
+  </div>
+</section>
